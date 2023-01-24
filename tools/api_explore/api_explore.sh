65c6c2bb574c14cb78e79b98f80eb6a2f769228b
Sergio Arroutbi <sarroutb@redhat.com>
2023-01-24 17:14:00 +0100

    Add api explore for frontend inspiration
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ tools/api_explore/api_explore.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Copyright 2022 Sergio Arroutbi <sarroutb@redhat.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Uncomment next line to dump verbose information in script execution:
+# set -x
+
+DEFAULT_K8SC="oc"
+test -z "${K8SC}" && K8SC=${DEFAULT_K8SC}
+
+DEFAULT_NAMESPACE="default"
+test -z "${NAMESPACE}" && NAMESPACE=${DEFAULT_NAMESPACE}
+
+function usage() {
+  echo
+  echo "ENDPOINT=tang-backend-tang TOKEN=\$(cat api_token.txt) NAMESPACE=ephemeral-rfymfc $0"
+  echo
+  exit "$2"
+}
+
+dumpInfo() {
+    echo "K8SC:${K8SC}"
+    echo "NAMESPACE:${NAMESPACE}"
+    echo "TOKEN=${TOKEN}"
+    echo "APISERVER=${APISERVER}"
+    echo "ENDPOINT=${ENDPOINT}"
+}
+
+installDeps() {
+    type jq 2>/dev/null 1>/dev/null || yum install -y jq 2>/dev/null 1>/dev/null
+}
+
+auth_curl() {
+    curl -o - \
+         --no-buffer \
+         --header "Connection: Upgrade" \
+         --header "Upgrade: websocket" \
+         --header "Host: example.com:80" \
+         --header "Origin: http://example.com:80" \
+         --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
+         --header "Sec-WebSocket-Version: 13" \
+         --header "Authorization: Bearer ${TOKEN}" \
+         "${1}"
+}
+
+while getopts "h" arg
+do
+  case "${arg}" in
+    h) usage "$0" 0
+       ;;
+    *) usage "$0" 1
+       ;;
+  esac
+done
+
+
+test -z "${PODNAME}" && PODNAME=$(guessPodName)
+test -z "${SERVICENAME}" && SERVICENAME=$(guessServiceName)
+
+APISERVER=$(oc config view --minify -o jsonpath='{.clusters[*].cluster.server}')
+
+# dumpInfo
+installDeps
+
+### Test API
+### auth_curl "${APISERVER}/openapi/v2"
+
+### Extract logs from pod
+### auth_curl "${APISERVER}/api/v1/namespaces/${NAMESPACE}/pods/${PODNAME}/log"
+
+### Extract status of pod
+#auth_curl "${APISERVER}/api/v1/namespaces/${NAMESPACE}/pods/${PODNAME}/status"
+
+### Command execution
+#auth_curl "${APISERVER}/api/v1/namespaces/${NAMESPACE}/pods/${PODNAME}/exec?command=/bin/bash&command=-c&command=${POD_COMMAND}&stdin=true&stderr=true&stdout=true&tty=true"
+
+### Extract status of service
+#auth_curl "${APISERVER}/api/v1/namespaces/${NAMESPACE}/services/${SERVICENAME}/status"
+#auth_curl "${APISERVER}/api/v1/namespaces/${NAMESPACE}/services/${SERVICENAME}"
+auth_curl "${APISERVER}/api/v1/namespaces/${NAMESPACE}/endpoints/${ENDPOINT}"
