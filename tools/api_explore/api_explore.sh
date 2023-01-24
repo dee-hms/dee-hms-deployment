94fc1518025523e65d9d53dd522f7aea6ec376c5
Sergio Arroutbi <sarroutb@redhat.com>
2023-01-24 17:17:00 +0100

    Remove unnecessary headers
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tools/api_explore/api_explore.sh
+++ tools/api_explore/api_explore.sh
@@ -45,12 +45,6 @@ installDeps() {
 auth_curl() {
     curl -o - \
          --no-buffer \
-         --header "Connection: Upgrade" \
-         --header "Upgrade: websocket" \
-         --header "Host: example.com:80" \
-         --header "Origin: http://example.com:80" \
-         --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
-         --header "Sec-WebSocket-Version: 13" \
          --header "Authorization: Bearer ${TOKEN}" \
          "${1}"
 }
