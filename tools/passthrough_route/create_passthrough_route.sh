f6fae21b66a2512eb4490132632dcd0c72842768
Sergio Arroutbi <sarroutb@redhat.com>
2023-08-09 20:25:47 +0200

    Include passthrough route tool (#82)
    Resolves: #81
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ tools/passthrough_route/create_passthrough_route.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright 2023 Sergio Arroutbi <sarroutb@redhat.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# TODO: parse this as input parameters
+name="tang-iam-proxy-passthrough-route"
+service="tang-iam-proxy-deployment"
+port="8000"
+
+oc create route passthrough "${name}" --service="${service}" --port="${port}"
