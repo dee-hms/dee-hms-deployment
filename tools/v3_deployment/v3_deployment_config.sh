459264809a232bd5f1c02519e4f5bd6449974386
Sergio Arroutbi <sarroutb@redhat.com>
2023-08-29 17:15:44 +0200

    Adapt to sqlite3 (instead of mysql)
    Resolves: #89
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tools/v3_deployment/v3_deployment_config.sh
+++ tools/v3_deployment/v3_deployment_config.sh
@@ -18,7 +18,8 @@ PKG_MGR="dnf"
 
 usage() {
     echo
-    echo "$1 -c <configFile> -d <dbHost:dbUser:dbPassword> [-t tang_podname (will be guessed if not provided)] [-k k8s_client (oc by default)] [-h] [-v] "
+    echo "$1 -c <configFile> [-d database_file (/var/lib/sqlite/tang_bindings.db)] [-p tang_iam_proxy (will be guessed by default)] "
+    echo "        [-t tang_podname (will be guessed if not provided)] [-k k8s_client (oc by default)] [-h] [-v] "
     echo
     exit "$2"
 }
@@ -35,18 +36,22 @@ installDependency() {
 }
 
 guessPodName() {
-    tang_podname=$("${k8s_client}" get pods | grep tang-backend | awk '{print $1}')
+    tang_podname=$("${k8s_client}" get pods | grep tang-server | awk '{print $1}')
     export tang_podname
 }
 
+guessProxyPodName() {
+    tang_iam_proxy_podname=$("${k8s_client}" get pods | grep tang-iam-proxy | awk '{print $1}')
+    export tang_iam_proxy_podname
+}
+
 checkParams() {
-    test -z "${dbhost}"       && usageMessage "$0" "Please, provide database host" 1
-    test -z "${dbuser}"       && usageMessage "$0" "Please, provide database user" 2
-    test -z "${dbpassword}"   && usageMessage "$0" "Please, provide database password" 3
-    test -z "${config_file}"  && usageMessage "$0" "Please, provide configuration file" 5
-    test -f "${config_file}"  || usageMessage "$0" "Please, provide existing configuration file" 6
+    test -z "${config_file}"  && usageMessage "$0" "Please, provide configuration file" 1
+    test -f "${config_file}"  || usageMessage "$0" "Please, provide existing configuration file" 2
     test -z "${k8s_client}"   && export k8s_client="oc"
     test -z "${tang_podname}" && guessPodName
+    test -z "${tang_iam_proxy_podname}" && guessProxyPodName
+    test -z "${database_file}" && export database_file="/var/lib/sqlite/tang_bindings.db"
 }
 
 detectPackageManager() {
@@ -63,11 +68,9 @@ dumpInfo() {
     if [ -n "${verbose}" ]; then
         echo "---------------------------------------------------------"
         echo "configuration_file:[${config_file}]"
-        echo "dbhost_user_password:[${dbhost_user_password}]"
-        echo "dbhost:${dbhost}"
-        echo "dbuser:${dbuser}"
-        echo "dbpassword:${dbpassword}"
+        echo "database_file:[${database_file}]"
         echo "tang_podname:${tang_podname}"
+        echo "tang_iam_proxy_podname:${tang_iam_proxy_podname}"
         echo "---------------------------------------------------------"
     fi
 }
@@ -79,26 +82,8 @@ installDependencies() {
     done
 }
 
-parseDbUserPassword() {
-    dbhost=$(echo "${dbhost_user_password}" | awk -F ':' '{print $1}')
-    export dbhost
-    dbuser=$(echo "${dbhost_user_password}" | awk -F ':' '{print $2}')
-    export dbuser
-    dbpassword=$(echo "${dbhost_user_password}" | awk -F ':' '{print $3}')
-    export dbpasword
-}
-
 databaseCommand() {
-    echo "$1" | mysql --host="${dbhost}" --user="${dbuser}" --password="${dbpassword}"
-}
-
-databaseCommandTangBindings() {
-    databaseCommand "USE tang_bindings; $1"
-}
-
-createDatabase() {
-    databaseCommand "CREATE DATABASE tang_bindings;" 2>/dev/null 1>/dev/null
-    databaseCommand "USE tang_bindings; create table bindings (spiffe_id VARCHAR(255) NOT NULL, tang_workspace VARCHAR(255) NOT NULL);" 2>/dev/null 1>/dev/null
+    oc exec "${tang_iam_proxy_podname}" -- sqlite3 "${database_file}" "$1" 2>/dev/null 1>/dev/null
 }
 
 insertEntries() {
@@ -109,7 +94,7 @@ insertEntries() {
         workspace=$(echo "${line}" | awk -F ',' '{print $1}')
         spiffe_id=$(echo "${line}" | awk -F ',' '{print $2}')
         echo "Inserting entry:workspace:[${workspace}];spire_id:[${spiffe_id}]"
-        databaseCommandTangBindings "insert into bindings (tang_workspace, spiffe_id) values ('${workspace}', '${spiffe_id}');"
+        databaseCommand "insert into bindings (tang_workspace, spiffe_id) values ('${workspace}', '${spiffe_id}');"
         "${k8s_client}" exec -i "${tang_podname}" -- /bin/bash -s <<EOF
 #!/bin/bash
 echo "${workspace},/var/db/${workspace}" >> /etc/socat-tang-filter.csv
@@ -117,18 +102,24 @@ EOF
     done< "${config_file}" 2>/dev/null 1>/dev/null
 }
 
+createTable() {
+    databaseCommand "create table if not exists bindings (spiffe_id VARCHAR(255) NOT NULL, tang_workspace VARCHAR(255) NOT NULL);"
+}
+
 while getopts "c:d:k:p:t:hv" arg
 do
     case "${arg}" in
         c) export config_file=${OPTARG}
            ;;
-        d) export dbhost_user_password=${OPTARG}
-           ;;
-        h) usage "$0" 0
+        d) export database_file=${OPTARG}
            ;;
         k) export k8s_client=${OPTARG}
            ;;
+        p) export tang_iam_proxy_podname=${OPTARG}
+           ;;
         t) export tang_podname=${OPTARG}
+           ;;
+        h) usage "$0" 0
            ;;
         v) export verbose=1
            ;;
@@ -137,10 +128,9 @@ do
     esac
 done
 
-parseDbUserPassword
 checkParams
 dumpInfo
 detectPackageManager
 installDependencies
-createDatabase
+createTable
 insertEntries
